f09c0eefaf32164fc9249f819aa46dbf36f39a96
Izuchuku <jeremiah.ortiz@edu.sait.ca>
2021-10-24 23:07:48 -0600

    Version 1.0 - QueueADT and StackADT interfaces implemented

---
+++ Assignment 2/src/utility/QueueADT.java
@@ -0,0 +1,113 @@
+package utility;
+import utility.Iterator;
+
+/**
+ * <p>
+ * The <code>QueueADT</code> interface is designed to be used as a basis for all
+ * the Queue data structures that will be developed for Assignment 2.
+ * The implementors of this interface will be required to add all the functionality.
+ * </p>
+ * 
+ * @param <E> The type of elements the stack holds.
+ */
+public interface QueueADT<E> 
+{
+	/**
+	 * <p>Precondition: <code>toAdd != null</code></p>
+	 * <p>Postcondition: Adds an element to the end of the queue.</p>
+	 * @param toAdd
+	 * 			Element to be added.
+	 * @return True if enqueue is successful.
+	 * @throws NullPointerException
+	 * 			If <code>toAdd == null</code>
+	 */
+	public Boolean enqueue(E toAdd) throws NullPointerException;
+	
+	/**
+	 * Precondition: List is not empty/null.
+	 * Postcondition: Removes the first element from a queue.
+	 * @return	True if dequeue is successful.
+	 * @throws NullPointerException
+	 * 			If element to be removed is null
+	 * 			(Queue is empty)
+	 */
+	public Boolean dequeue() throws NullPointerException;
+	
+	/**
+	 * Precondition: List is not empty/null
+	 * Postcondition: Retrieves the first element from a queue
+	 * @return First element in a queue
+	 * @throws NullPointerException
+	 * 			If retrieved element is null
+	 * 			(Queue is empty)
+	 */
+	public E peek() throws NullPointerException;
+	
+	/**
+	 * Precondition: None.
+	 * <p>Postcondition: Returns the size of the queue.</p>
+	 * @return Size of the queue
+	 */
+	public int size();
+	
+	/**
+	 * Precondition: None.
+	 * <p>Postcondition: Determines if the queue is empty or not.</p>
+	 * @return True if queue is empty.
+	 */
+	public boolean isEmpty();
+	
+	/**
+	 * Precondition: None.
+	 * <p>Postcondition: Removes all elements from the queue.</p>
+	 */
+	public void dequeueAll();
+	
+	/**
+	 * Precondition: <code>that</code> != null.
+	 * <p>Postcondition: Determines if two queues are equal.</p>
+	 * @param that 
+	 * 			Queue to be compared to.
+	 * @return True if the two stacks are equal.
+	 * @throws NullPointerException
+	 * 			If the specified element is <code>null</code> and the stack
+	 * 			implementation does not support having <code>null</code>
+	 * 			elements.
+	 */
+	public boolean equals(QueueADT<E> that) throws NullPointerException;
+	
+	/**
+	 * Returns an array containing all of the elements in this queue in proper
+	 * sequence. Obeys the general contract of the 
+	 * <code>java.util.Collection.toArray()</code> method.
+	 * 
+	 * @return An array containing all of the elements in this queue in proper
+	 * 			sequence.
+	 */
+	public Object[] toArray();
+	
+	/**
+	 * Returns an array containing all of the elements in this queue in proper
+	 * sequence; the runtime type of the returned array is that of the specified
+	 * array. Obeys the general contract of the
+	 * <code>java.util.Collection.toArray(Object [])</code> method.
+	 * 
+	 * @param copy
+	 *			The array into which the elements of this queue are to be
+	 * 			stored.
+	 * @return An array containing the elements of this queue.
+	 * @throws NullPointerException
+	 * 			If the specified array is <code>null</code>.
+	 */
+	public E[] toArray(E[] copy) throws NullPointerException;
+	
+	/**
+	 * Returns an iterator over the elements in this queue, in proper sequence.
+	 * 
+	 * @return An iterator over the elements in this queue, in proper sequence.
+	 * 			NB: The return is of type 
+	 * 			<code>linearUtilities.Iterator<E></code>,
+	 * 			not <code>java.util.Iterator</code>.
+	 */
+	public Iterator<E> iterator();
+}
---
+++ Assignment 2/src/utility/StackADT.java
@@ -0,0 +1,147 @@
+package utility;
+import utility.Iterator;
+
+/**
+ * <p>
+ * The <code>StackADT</code> interface is designed to be used as a basis for all
+ * the Stack data structures that will be developed for Assignment 2.
+ * The implementors of this interface will be required to add all the functionality.
+ * </p>
+ * 
+ * @param <E> The type of elements the stack holds.
+ */
+public interface StackADT<E> extends Iterator
+{
+	/**
+	 * Precondition: Element toAdd != null.
+	 * <p>Postcondition: Adds an element to the top of the stack.</p>
+	 * @param toAdd 
+	 * 			Element to be added to the stack.
+	 * @return True if element is added successfully.
+	 * @throws NullPointerException
+	 * 			If the specified element is <code>null</code> and the stack
+	 * 			implementation does not support having <code>null</code>
+	 * 			elements.
+	 */
+	public Boolean push(E toAdd) throws NullPointerException;
+	
+	/**
+	 * Precondition: List is not empty/null.
+	 * <p>Postcondition: Removes the element at the top of the stack.</p>
+	 * @return True if element is successfully removed.
+	 * @throws NullPointerException
+	 * 			If the element to be deleted is null
+	 * 			(Stack is empty)
+	 */
+	public Boolean pop() throws NullPointerException;
+	
+	/**
+	 * Precondition: List is not empty/null.
+	 * <p>Postcondition: Retrieves the element at the top of the stack.</p>
+	 * @return Element on the top of the list
+	 * @throws IndexOutOfBoundsException
+	 * 			If the index is out of range.
+	 * 			i.e. (index < 0)
+	 * @throws NullPointerException
+	 * 			If the specified element is <code>null</code> and the stack
+	 * 			implementation does not support having <code>null</code>
+	 * 			elements.
+	 */
+	public E peek() throws NullPointerException, IndexOutOfBoundsException;
+	
+	/**
+	 * Precondition: None.
+	 * <p>Postcondition: Returns the size of the stack.</p>
+	 * @return Size of the stack
+	 */
+	public int size();
+	
+	/**
+	 * Precondition: None.
+	 * <p>Postcondition: Determines if the stack is empty or not.</p>
+	 * @return True if stack is empty.
+	 */
+	public boolean isEmpty();
+	
+	/**
+	 * Precondition: None.
+	 * <p>Postcondition: Removes all elements from the stack.</p>
+	 */
+	public void clear();
+	
+	/**
+	 * Precondition: <code>that</code> != null.
+	 * <p>Postcondition: Determines if two stacks are equal.</p>
+	 * @param that 
+	 * 			Stack to be compared to.
+	 * @return True if the two stacks are equal.
+	 * @throws NullPointerException
+	 * 			If the specified element is <code>null</code> and the stack
+	 * 			implementation does not support having <code>null</code>
+	 * 			elements.
+	 */
+	public boolean equals(StackADT<E> that) throws NullPointerException;
+	
+	/**
+	 * Precondition: <code>toFind != null</code>
+	 * <p>Postcondition: Returns position of element <code>toFind</code>. </p>
+	 * <p>Top of the stack is at position 1.</p>
+	 * @param toFind
+	 * 			element to be found.
+	 * @return Position of specified element.
+	 * @throws NullPointerException
+	 * 			If the specified element is <code>null</code> and the stack
+	 * 			implementation does not support having <code>null</code>
+	 * 			elements.
+	 */
+	public int search(E toFind) throws NullPointerException;
+	
+	/**
+	 * Precondition: <code>toFind != null</code>
+	 * <p>Postcondition: Determines if stack contains the specified element.</p>
+	 * @param toFind
+	 * 			element to be found.
+	 * @return True if element is found
+	 * @throws NullPointerException
+	 * 			If the specified element is <code>null</code> and the stack
+	 * 			implementation does not support having <code>null</code>
+	 * 			elements.
+	 */
+	public boolean contains(E toFind) throws NullPointerException;
+	
+	/**
+	 * Returns an array containing all of the elements in this stack in proper
+	 * sequence. Obeys the general contract of the 
+	 * <code>java.util.Collection.toArray()</code> method.
+	 * 
+	 * @return An array containing all of the elements in this stack in proper
+	 * 			sequence.
+	 */
+	public Object[] toArray();
+	
+	/**
+	 * Returns an array containing all of the elements in this stack in proper
+	 * sequence; the runtime type of the returned array is that of the specified
+	 * array. Obeys the general contract of the
+	 * <code>java.util.Collection.toArray(Object [])</code> method.
+	 * 
+	 * @param copy
+	 *			The array into which the elements of this list are to be
+	 * 			stored.
+	 * @return An array containing the elements of this list.
+	 * @throws NullPointerException
+	 * 			If the specified array is <code>null</code>.
+	 */
+	public E[] toArray(E[] copy) throws NullPointerException;
+	
+	/**
+	 * Returns an iterator over the elements in this stack, in proper sequence.
+	 * 
+	 * @return An iterator over the elements in this stack, in proper sequence.
+	 * 			NB: The return is of type 
+	 * 			<code>linearUtilities.Iterator<E></code>,
+	 * 			not <code>java.util.Iterator</code>.
+	 */
+	public Iterator<E> iterator();
+	
+}
